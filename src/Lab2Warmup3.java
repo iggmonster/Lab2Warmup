d193957dda927b5844cd25f59a72369db21843a6
iggmonster <112781295+iggmonster@users.noreply.github.com>
2022-09-07 15:50:12 -0400

    ,:)

--- src/Lab2Warmup3.java
+++ src/Lab2Warmup3.java
@@ -1,15 +1,29 @@
+import java.text.DecimalFormat;
 import java.util.Scanner;
 
 public class Lab2Warmup3 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.println("Enter investment amount:");
-        public double calculateFutureInvestmentValue
-         ( double investmentAmount,
-        double annualInterestRate, double numberOfYears ) {
+        System.out.print("Enter investment amount: ");
+        Double investmentAmount = Double.valueOf(input.nextLine());
+        System.out.print("Enter yearly interest rate: ");
+        Double annualInterestRate = Double.valueOf(input.nextLine());
+        System.out.print("Enter number of years: ");
+        Double numberOfYears = Double.valueOf(input.nextLine());
+        calculateFutureInvestmentValue(investmentAmount, annualInterestRate, numberOfYears);
+
+        System.out.println("Accumulated value is " + (calculateFutureInvestmentValue(investmentAmount, annualInterestRate, numberOfYears)));
+
+// futureInvestment Value = investmentAmount ∗ (1 + annualInterestRate/12) ^ numberOfYears∗12
+    }
 
 
-        }
+    public static double calculateFutureInvestmentValue ( double investmentAmount,
+    double annualInterestRate, double numberOfYears ) {
+        Double futureInvestmentValue = Double.valueOf(0);
+        futureInvestmentValue = Math.pow (investmentAmount * (1 + annualInterestRate / 12) , (numberOfYears * 12));
+
+        return futureInvestmentValue;
     }
 }
 
